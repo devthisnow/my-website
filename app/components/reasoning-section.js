2d11a0d92ac2e36c548af74bc56f292c27fbfcb3
Ilia Dev <bridatal@gmail.com>
2024-07-19 19:44:45 +0400

    CUP 8.4: Skillset accordion designed

--- app/components/reasoning-section.js
+++ app/components/reasoning-section.js
@@ -2,17 +2,17 @@
 export default function ReasonsToWork(props) {
     return (
         <section className="flex flex-col justify-center items-center w-full min-h-[1080px] mt-[160px] ">
-            <div className="w-8/12 mb-2 text-[28px] border-b-4 text-center border-black">
-                <h2 className="text-[107px] font-semibold mb-4 capitalize ">Why to work with me</h2>
-                <ul className="mb-[50px] text-[28px] text-center max-h-10 overflow-y-scroll snap-y snap-mandatory ">
-                    <li className="snap-start animate-scroll-me [animation-timeline:view()]">Your needs are what my workflow is built around.</li>
-                    <li className="snap-start animate-scroll-me [animation-timeline:view()] h-full">My approach is based on collaboration and professionalism.</li>
-                    <li className="snap-start animate-scroll-me [animation-timeline:view()]">Team-player ready to join your team.</li>
-                    <li className="snap-start animate-scroll-me [animation-timeline:view()]">Emotionally stable and capable of hard work.</li>
-                    <li className="snap-start animate-scroll-me [animation-timeline:view()]">Successfully launched many projects across the globe.</li>
-                    <li className="snap-start animate-scroll-me [animation-timeline:view()]">Great communication and on-time delivery.</li>
-                </ul>
-            </div>
+
+            <h2 className="text-[107px] font-semibold mb-4 capitalize ">Why to work with me</h2>
+            <ul className="w-full mb-[50px] text-[28px] text-center max-h-20 overflow-y-scroll [scrollbar-width: none] snap-y snap-mandatory hover:animate-scroll-me ">
+                <li className="snap-start animate-scroll-me [animation-timeline:view()]">Your needs are what my workflow is built around.</li>
+                <li className="snap-start animate-scroll-me [animation-timeline:view()] h-full">My approach is based on collaboration and professionalism.</li>
+                <li className="snap-start animate-scroll-me [animation-timeline:view()]">Team-player ready to join your team.</li>
+                <li className="snap-start animate-scroll-me [animation-timeline:view()]">Emotionally stable and capable of hard work.</li>
+                <li className="snap-start animate-scroll-me [animation-timeline:view()]">Successfully launched many projects across the globe.</li>
+                <li className="snap-start animate-scroll-me [animation-timeline:view()]">Great communication and on-time delivery.</li>
+            </ul>
+            <div className="w-8/12 mb-2 text-[28px] border-b-4 text-center border-black"></div>
         </section>
     );
 };
--- app/components/skillset-section.js
+++ app/components/skillset-section.js
@@ -3,12 +3,63 @@ import Image from "next/image";
 
 export default function Skillset(props) {
     return (
-        <section className="grid grid-cols-12 grid-rows-3 gap-x-[20px] w-full min-h-[890px] max-h-[890px] pl-[80px] mt-[160px] ">
-            <h2 className="col-span-3 text-[107px] font-semibold mb-2">Skillset</h2>
-            <div className="col-span-7 col-start-6 row-start-2 w-full bg-mygray">
-
-            </div>
-            <p className="col-span-6 col-start-2 row-start-3 font-semibold text-[28px] leading-2 pr-10">Leveraging my experience in navigating the constraints and demands of the space industry, I now bring a unique perspective to the world of online platforms, meticulously crafting websites that are not only visually stunning and user-friendly but also highly efficient, scalable, and tailored to the specific needs of diverse clientele.</p>
-        </section>
+        <section className="grid grid-cols-12 grid-rows-1 gap-x-[20px] w-full px-[80px] mt-[160px] ">
+            <h2 className="col-span-3 text-[107px] font-semibold leading-none ">Skillset</h2>
+            <div className="col-span-7 col-start-6 row-start-2 w-full min-h-full ">
+                <div className="flex flex-row justify-start items-center h-[130px] border-t-[3px] max-h-[800px] border-black px-[30px]">
+                    <details name="reqs" className="grow open:max-h-[800px] open:[transition: max-height 400ms ease-out] ">
+                        <summary className="text-[28px] font-semibold cursor-pointer list-none ">Languages</summary>
+                        <ul className="[transition: max-height 400ms ease-out]">
+                            <li>Javascript</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>Python</li>
+                            <li>C++</li>
+                        </ul>
+                    </details>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={0.5} stroke="currentColor" className="size-[100px]">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </div>
+                <div className="flex flex-row justify-start items-center h-[130px] border-t-[3px] max-h-[800px] border-black px-[30px]">
+                    <details name="reqs" className="grow open:max-h-[800px] open:[transition: max-height 400ms ease-out] ">
+                        <summary className="text-[28px] font-semibold cursor-pointer list-none">Frameworks / Libraries / Others</summary>
+                        <ul>
+                            <li>React.js</li>
+                            <li>Node.js</li>
+                            <li>React Query</li>
+                            <li>Next.js </li>
+                            <li>Express.js</li>
+                            <li>React Testing Library</li>
+                            <li>Web3</li>
+                            <li>Redux</li>
+                            <li>Tailwind CSS</li>
+                            <li>Lottie</li>
+                        </ul>
+                    </details>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={0.5} stroke="currentColor" className="size-[100px]">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </div>
+                <div className="flex flex-row justify-start items-center h-[130px] border-y-[3px] max-h-[800px] border-black px-[30px]">
+                    <details name="reqs" className="grow open:max-h-[800px] open:[transition: max-height 400ms ease-out] ">
+                        <summary className="text-[28px] font-semibold cursor-pointer list-none">Others</summary>
+                        <ul>
+                            <li>Adobe XD</li>
+                            <li>Adobe Photoshop</li>
+                            <li>Adobe AfterEffects</li>
+                            <li>Adobe PremierePro</li>
+                            <li>Cinema 4D</li>
+                            <li>My approach is based on collaboration and professionalism.</li>
+                            <li>Team-player ready to join your team.</li>
+                        </ul>
+                    </details>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={0.5} stroke="currentColor" className="size-[100px]">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </div>
+            </div >
+            <p className="col-span-6 col-start-2 row-start-3 font-semibold text-[28px] leading-2 pr-10 mt-[210px]">Leveraging my experience in navigating the constraints and demands of the space industry, I now bring a unique perspective to the world of online platforms, meticulously crafting websites that are not only visually stunning and user-friendly but also highly efficient, scalable, and tailored to the specific needs of diverse clientele.</p>
+        </section >
     );
 };
